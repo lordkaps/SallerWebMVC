bca68f86ce902f6a8fe14cb8f87b5e9e236137ca
Augusto de Paulo Santos <augusto.santos@aliare.co>
2022-12-28 13:39:27 -0300

    Add Controller de Departamento com um index de teste

---
+++ SallerWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SallerWebMVC.Models;
+using System.Collections.Generic;
+
+namespace SallerWebMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>
+            {
+                new Department { Id = 1, Name = "Eletronics" },
+                new Department { Id = 2, Name = "Fashion" }
+            };
+
+            return View(list);
+        }
+    }
+}
---
+++ SallerWebMVC/Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace SallerWebMVC.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
