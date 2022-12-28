ffc67699340069db019582297f294e1734802ecf
Augusto de Paulo Santos <augusto.santos@aliare.co>
2022-12-28 15:56:54 -0300

    Alterado Versão core de 2.1 para 2.2

---
+++ SallerWebMVC/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SallerWebMVC.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
