3a6bb501503f531a771203566395d5da62590fcf
Augusto de Paulo Santos <augusto.santos@aliare.co>
2022-12-28 16:26:56 -0300

    Realizado add CRUD Scaffolding

---
+++ SallerWebMVC/Data/SallerWebMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SallerWebMVC.Models;
+
+namespace SallerWebMVC.Data
+{
+    public class SallerWebMVCContext : DbContext
+    {
+        public SallerWebMVCContext (DbContextOptions<SallerWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SallerWebMVC.Models.Department> Department { get; set; }
+    }
+}
--- SallerWebMVC/Startup.cs
+++ SallerWebMVC/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SallerWebMVC.Data;
 
 namespace SallerWebMVC
 {
@@ -33,6 +35,9 @@ namespace SallerWebMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SallerWebMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SallerWebMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
