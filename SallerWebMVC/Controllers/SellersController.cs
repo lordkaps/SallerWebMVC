95c033f4ff277c3c80dceb5433e3e4b6353852b7
Augusto de Paulo Santos <augusto.santos@aliare.co>
2022-12-29 16:51:31 -0300

    Adicionado forma de apagar vendedor do banco

--- SallerWebMVC/Controllers/SellersController.cs
+++ SallerWebMVC/Controllers/SellersController.cs
@@ -36,5 +36,29 @@ namespace SallerWebMVC.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var seller = _sellerService.FindById(id.Value);
+            if (seller == null)
+            {
+                return NotFound();
+            }
+
+            return View(seller);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- SallerWebMVC/Services/SellerService.cs
+++ SallerWebMVC/Services/SellerService.cs
@@ -24,5 +24,17 @@ namespace SallerWebMVC.Services
             _context.Add(seller);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(s => s.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var seller = _context.Seller.Find(id);
+            _context.Seller.Remove(seller);
+            _context.SaveChanges();
+        }
     }
 }
