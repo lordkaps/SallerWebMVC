3009174b89ef9cd17b885adfe71d42479887e9d7
Augusto de Paulo Santos <augusto.santos@aliare.co>
2023-01-19 09:29:49 -0300

    Adicionado validação dos dados de Seller

--- SallerWebMVC/Controllers/SellersController.cs
+++ SallerWebMVC/Controllers/SellersController.cs
@@ -38,6 +38,12 @@ namespace SallerWebMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -104,6 +110,13 @@ namespace SallerWebMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit (int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             if(id != seller.Id) 
             {
                 return RedirectToAction(nameof(Error), new { message = "Id não correspondente." });
--- SallerWebMVC/Models/Seller.cs
+++ SallerWebMVC/Models/Seller.cs
@@ -10,22 +10,31 @@ namespace SallerWebMVC.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "{0} obrigatorio.")]
+        [StringLength(60, MinimumLength = 4, ErrorMessage = "{0} Tamanho entre {2} e {1}")]
         [Display(Name = "Nome")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} requerido.") ]
+        [EmailAddress(ErrorMessage = "Coloque um email válido.")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} necessario.")]
         [Display(Name = "Data de nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} precisa ser informado.")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} deve estar entre {1} e {2}")]
         [Display(Name = "Salario Base")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller() { }
