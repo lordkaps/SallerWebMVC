c4cc93db5541dedc052f524ccbdfbb448b402f00
Augusto de Paulo Santos <augusto.santos@aliare.co>
2023-02-02 10:31:30 -0300

    Implementado busca simples dos registros de vendas

---
+++ SallerWebMVC/Services/SalesRecordService.cs
@@ -0,0 +1,40 @@
+﻿using SallerWebMVC.Data;
+using SallerWebMVC.Models;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace SallerWebMVC.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SallerWebMVCContext _context;
+
+        public SalesRecordService(SallerWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync (DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+    }
+}
--- SallerWebMVC/Startup.cs
+++ SallerWebMVC/Startup.cs
@@ -40,6 +40,7 @@ namespace SallerWebMVC
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
             services.AddScoped<DepartmentService>();
+            services.AddScoped<SalesRecordService>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
